a0977042d9e37d73faa6fac15d35838b20f02582
Batbold Gankhuyag <bgankhuyag@mail.roanoke.edu>
2021-07-29 17:54:51 +0800

    nav

--- src/components/nav/index.js
+++ src/components/nav/index.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles.css'
 
 const Nav = () => {
+  const [scroll, setScroll] = useState(false);
+
+  window.addEventListener("scroll", () => {
+    setScroll(window.scrollY > 50);
+    // console.log(scroll);
+  });
+
   return (
-    <div className="nav">
+    <div className={scroll ? "nav" : "nav filled"}>
       <a>About</a>
+      <a>Projects</a>
+      <a>Contact</a>
     </div>
   )
 }
