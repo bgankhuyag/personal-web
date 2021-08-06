eb0cf8cb1bba7984ef2237cc37d207685c4f7d75
Batbold Gankhuyag <bgankhuyag@mail.roanoke.edu>
2021-08-06 17:15:04 +0800

    contact & projects files

--- src/views/home.js
+++ src/views/home.js
@@ -50,8 +50,6 @@ const Home = () => {
         <Header />
         <About />
         <Experience />
-        <Header />
-        <Header />
       </div>
       <Menu handleClose={handleClose} />
     </div>
