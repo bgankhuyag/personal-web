2d1a2e3ca5e5e69384ecf7cec65d676cf3200c0b
Batbold Gankhuyag <bgankhuyag@mail.roanoke.edu>
2022-01-05 20:34:42 -0500

    Projects style

--- src/components/projects/index.js
+++ src/components/projects/index.js
@@ -10,21 +10,24 @@ const Projects = () => {
 
   const info = (item) => (
     <div className="project-info">
-      <h3 className="project-name">{item['name']}</h3>
+      <h3 className="project-name"><a href="#" className="link">{item['name']}</a></h3>
       <div className="description">{item['description']}</div>
       {
         item['github'] ?
-        <span><a href={item['github']} className="link project-link" target="_blank"><i className="fab fa-github"></i></a> &nbsp;&nbsp;</span>
+        <span><a href={item['github']} className="link project-link" target="_blank" rel="noreferrer"><i className="fab fa-github"></i></a> &nbsp;&nbsp;</span>
         : null
       }
-      <span><a href={item['github']} className="link project-link" target="_blank"><i class="fas fa-external-link-alt"></i></a></span>
-
+      {
+        item['link'] ?
+        <span><a href={item['github']} className="link project-link" target="_blank" rel="noreferrer"><i class="fas fa-external-link-alt"></i></a></span>
+        : null
+      }
     </div>
   )
 
   const image = (item) => (
     <div className="project-image-container" style={{marginBottom: 0}}>
-      <img className="project-image" src={item['image']} />
+      <img className="project-image" src={item['image']} alt={item['name']}/>
     </div>
   )
 
@@ -38,7 +41,7 @@ const Projects = () => {
         </div>
         <div className="project-tools">
         {
-          item['tools'].map((tool, index) => (<span key={'project' + item['name'] + index}>{tool} &nbsp;</span>))
+          item['tools'].map((tool, index) => (<span key={'project' + item['name'] + index}>{tool} </span>))
         }
         </div>
       </div>
